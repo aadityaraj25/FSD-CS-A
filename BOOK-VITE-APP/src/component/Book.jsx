d0360ac7f5dfa647689f14bd983d12586e9fe074
Aditya Raj Sharma <arj25022005@gmail.com>
2025-10-31 13:06:26 +0530

    increment and decrement

--- BOOK-VITE-APP/src/component/Book.jsx
+++ BOOK-VITE-APP/src/component/Book.jsx
@@ -2,6 +2,13 @@ import React, { useState } from 'react'
 import './Book.css'
 const Book = ({book}) => {
     const [count,setCount] = useState(0);
+    function increment(){
+        setCount(count+1);
+    }
+    function decrement(){
+        if(count>0)
+        setCount(count-1);
+    }
     function handleAddtoCart(){
         alert(`${book.title} Added Successfully`)
     }
@@ -11,9 +18,9 @@ const Book = ({book}) => {
       <h3>Title: {book.title}</h3>
       <h4>Price: ₹{book.price}/-</h4>
       <div>
-        <button>-</button>
+        <button onClick={decrement}>-</button>
         <span>{count}</span>
-        <button>+</button>
+        <button onClick={increment}>+</button>
       </div>
       <button onClick={handleAddtoCart}>Add to Cart</button>
     </div>
